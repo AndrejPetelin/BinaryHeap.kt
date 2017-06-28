61a49dabb68561a8086b89fd9408c6ccd6e6d7c8
Andrej Petelin <petelin.andrej@amis.net>
2017-06-28 21:16:24 +0200

    Add files via upload

--- binaryHeap.kt
+++ binaryHeap.kt
@@ -1,26 +1,35 @@
 /**
  * BinaryHeap.kt
  * A binary heap data structure.
- * TODO - add pushArray, popArray, peekArray
+ *
+ * TODO - add pushList, popList, peekList
  * Created by andrej on 18.6.2017.
+ *
+ *
  */
 
-class BinaryHeap<T>(private var comp: (x: T, y: T) -> Boolean) {
+
+package BinaryHeap
+
+
+class BinaryHeap<T>(private var comp: (T, T) -> Boolean) {
 
     private var _data: MutableList<T> = mutableListOf<T>()
+
     val size: Int
         get() = _data.size
 
     fun isEmpty(): Boolean = _data.isEmpty()
 
+    /**
+     * adds element to BinaryHeap and reorder _data to maintain heap consistency
+     */
     fun push(x: T) {
         _data.add(x)
         var index: Int = size - 1
 
         while (index > 0) {
             val parent = parentIndex(index)
-
-            // swap when comparison true
             if(comp(_data[index], _data[parent])) {
                 swapAt(index, parent)
                 index = parent
@@ -30,6 +39,10 @@ class BinaryHeap<T>(private var comp: (x: T, y: T) -> Boolean) {
         }
     }
 
+
+    /**
+     * remove "smallest" element from BinaryHeap and reorder _data to maintain heap consistency
+     */
     fun pop(): T? {
         if (isEmpty()) return null
 
@@ -41,7 +54,6 @@ class BinaryHeap<T>(private var comp: (x: T, y: T) -> Boolean) {
 
         while (index < size) {
             val childIndex = minChildIndex(index)
-
             if (childIndex != null && comp(_data[childIndex], _data[index])) {
                 swapAt(index, childIndex)
                 index = childIndex
@@ -52,7 +64,16 @@ class BinaryHeap<T>(private var comp: (x: T, y: T) -> Boolean) {
         return ret
     }
 
+
+    /**
+     * shows last element of heap without removal
+     */
     fun peek(): T? = if (!isEmpty()) _data[0] else null
+
+
+    /**
+     * helper functions for internal book-keeping
+     */
 
     private fun parentIndex(n: Int) = (n + 1) / 2 - 1
 
@@ -60,7 +81,6 @@ class BinaryHeap<T>(private var comp: (x: T, y: T) -> Boolean) {
         val childR = (n + 1) * 2
         val childL = childR - 1
 
-        // null when no children exist, otherwise check if right child exists, return the index of max
         if (childL >= size) return null
         else if (childR < size && comp(_data[childR], _data[childL])) return childR
         else return childL
@@ -73,14 +93,40 @@ class BinaryHeap<T>(private var comp: (x: T, y: T) -> Boolean) {
     }
 }
 
+
+
+/**
+ * create BinaryHeap of Comparable types, heap will be sorted according to natural ordering of elements
+ */
 fun <T: Comparable<T>> binaryHeap(): BinaryHeap<T> {
     return BinaryHeap<T>({x: T, y: T -> x < y})
 }
 
-fun <T> binaryHeap(comp: (x: T, y: T) -> Boolean): BinaryHeap<T> {
+/**
+ * create BinaryHeap of any kind of types, ordering will be done according to provided predicate function
+ */
+fun <T> binaryHeap(comp: (T, T) -> Boolean): BinaryHeap<T> {
     return BinaryHeap<T>(comp)
 }
 
+/**
+ * create a BinaryHeap from data which has a natural order
+ */
+fun <T: Comparable<T>> toBinaryHeap(vararg args: T): BinaryHeap<T> {
+    val ret = binaryHeap<T>()
+    for (x in args) ret.push(x)
+    return ret
+}
+
+
+/**
+ * create a BinaryHeap from data that is ordered according to provided comparison function
+ */
+fun <T> toBinaryHeap(comp: (T, T) -> Boolean, vararg args: T): BinaryHeap<T> {
+    val ret = BinaryHeap<T>(comp)
+    for (x in args) ret.push(x)
+    return ret
+}
 
 fun main(args: Array<String>) {
     val pq = binaryHeap<Int>({ x, y -> x > y })
@@ -95,6 +141,9 @@ fun main(args: Array<String>) {
     pq.push(666)
     pq.push(-123)
 
+    val pq2 = pq
+
+    println(pq.peek())
     println(pq.pop())
     println(pq.pop())
     println(pq.pop())
@@ -106,6 +155,12 @@ fun main(args: Array<String>) {
     println(pq.pop())
     println(pq.pop())
     println(pq.pop())
-    println(pq.pop())
+
+    println()
+    println(pq2.peek())
+    println()
+
+    val pq3 = toBinaryHeap(2,3,4,1,12,16,22,42,-1234,666,8)
+    while (!pq3.isEmpty()) println(pq3.pop())
 
 }
